c1436f868e4e1f57a893fa329b46e331ca3a07a8
Kidz <khoipro1907@gmail.com>
2025-02-18 09:34:22 +0700

    update

--- src/config/firebaseService.js
+++ src/config/firebaseService.js
@@ -1,4 +1,4 @@
-import { collection, getDocs, getDoc, setDoc, doc } from "firebase/firestore";
+import { collection, getDocs, getDoc, setDoc, addDoc, doc } from "firebase/firestore";
 import { db } from "./firebaseConfig";
 
 const getTimelineData = async () => {
@@ -20,6 +20,49 @@ const getTimelineData = async () => {
     }
 };
 
+const addTimelineVN = async (title, description, date) => {
+    try {
+        const docRef = await addDoc(collection(db, "timeline_vn"), {
+            title,
+            description,
+            date,
+        });
+        console.log("Vietnamese timeline entry added with ID:", docRef.id);
+    } catch (error) {
+        console.error("Error adding timeline entry:", error);
+    }
+};
+
+const portfolioData = {
+    name: "BÙI THANH KHÔI",
+    title: "Junior Developer",
+    bio: "Passionate about building modern web and mobile applications.",
+    yoe: {
+        mobile: "1+ year of experience in mobile developing (6 months of Kotlin and 6+ months of Flutter)",
+        web: "1,5 year of experience in web developing(Reactjs)",
+        other: "Experiencing in backend(api handle with Nodejs and MongoDb,Firebase as cloud db) because i worked as full-stack developer in multiple projects"
+    },
+    contact: {
+        email: "khoipro1907@gmail.com",
+        phone: "+(84)935085046",
+        linkedin: "https://www.linkedin.com/in/khoi-bui-3b82391b5/",
+        github: "https://github.com/kidz1907"
+    },
+    hobby: {
+        sport: "I enjoy playing a wide variety of sports: badminton, swimming, chess, poker ...",
+        code: "I some of the leisure time to do coding exercise with Leetcode. I also self-learn game developing with Unity because i have interest in gaming industry as well",
+        games: "I liked kind of games from casual to competitive but mostly puzzle solving game.",
+    }
+};
+
+const addBasicInfoVN = async (data) => {
+    try {
+        await setDoc(doc(db, "portfolio", "basicInfo_vn"), data);
+        console.log("Vietnamese basic info updated successfully!");
+    } catch (error) {
+        console.error("Error adding basicInfo_vn:", error);
+    }
+};
 const getPortfolioData = async () => {
     try {
         const portfolioRef = doc(db, "portfolio", "basicInfo"); // Reference to "basicInfo" document
